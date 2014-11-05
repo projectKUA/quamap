5e661d4b33536b0b6191050bedf70c3d82b7de69
ucchi <sabu3260@gmail.com>
2014-11-05 10:54:53 +0000

    merge conflicts.

---
+++ app/assets/javascripts/googlemap.js
@@ -0,0 +1,36 @@
+var styles = [
+	{
+		"featureType": "administrative.country",
+		"stylers": [
+			{ "visibility": "off" }
+		]
+	},{
+		"elementType": "labels",
+		"stylers": [
+			{ "visibility": "off" }
+		]
+	},{
+		"featureType": "landscape",
+		"stylers": [
+			{ "color": "#ffffff" }
+		]
+	},{
+		"featureType": "poi",
+		"stylers": [
+			{ "visibility": "off" }
+		]
+	},{
+		"featureType": "road",
+		"elementType": "geometry.fill",
+		"stylers": [
+			{ "visibility": "on" },
+			{ "color": "#ffffff" }
+		]
+	},{
+		"elementType": "geometry.stroke",
+		"stylers": [
+			{ "color": "#cfcfd0" }
+		]
+	}
+];
+
