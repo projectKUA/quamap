f484854ac491e2b416c3fbefd19938b054bb7fea
syanao <kubota.docomo@gmail.com>
2014-11-07 13:10:00 +0000

    ref #40 Bug fix

--- app/models/qua.rb
+++ app/models/qua.rb
@@ -5,7 +5,7 @@ class Qua < ActiveRecord::Base
   def self.center
     random = Qua.random
     center = random[0] unless random.nil? 
-    if !center.nil? && center["latitude"] == nil || center["longitude"] == nil
+    if center.nil? || center["latitude"] == nil || center["longitude"] == nil
       q = Qua.new
       q.latitude = 38.743224
       q.longitude = 140.715955
